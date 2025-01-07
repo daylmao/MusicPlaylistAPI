29d4b1c21c4d399ff6f5007ca7a3b06d5b6d573f
Dayron <dayronbp06@gmail.com>
2025-01-07 12:28:38 -0400

    add song DTOs

--- MusicPlaylistAPI.Core.Application/DTOs/Song/SongDTO.cs
+++ MusicPlaylistAPI.Core.Application/DTOs/Song/SongDTO.cs
@@ -9,6 +9,8 @@ namespace MusicPlaylistAPI.Core.Application.DTOs.Song
     public class SongDTO
     {
         public string? Artist { get; set; }
+        public string? Title { get; set; }
+
         public string? Genre { get; set; }
         public float Duration { get; set; }
     }
