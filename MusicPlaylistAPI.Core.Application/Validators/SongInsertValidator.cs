f7cc85607dacc0455462ad5f02849f8f76f9bea8
Dayron <dayronbp06@gmail.com>
2025-01-07 22:47:25 -0400

    create validators

---
+++ MusicPlaylistAPI.Core.Application/Validators/SongInsertValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlaylistAPI.Core.Application.Validators
+{
+    public class SongInsertValidator
+    {
+    }
+}
