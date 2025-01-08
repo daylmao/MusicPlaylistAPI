7d53b29ab3c9215ea252a464333b528513958552
Dayron <dayronbp06@gmail.com>
2025-01-08 15:22:45 -0400

    update name

---
+++ MusicPlaylistAPI.Core.Application/Validators/UpdatePlaylist.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MusicPlaylistAPI.Core.Application.DTOs.Playlist;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlaylistAPI.Core.Application.Validators
+{
+    public class UpdatePlaylist: AbstractValidator<PlaylistUpdateDTO>
+    {
+        public UpdatePlaylist()
+        {
+            RuleFor(b => b.Name).MaximumLength(50).WithMessage("Name must not exceed 50 characters");
+
+            RuleFor(b => b.Description)
+                .MaximumLength(100).WithMessage("Description must not exceed 100 characters");
+
+            //falta validacion de song
+
+            RuleFor(b => b.DateCreated)
+                .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("DateCreated cannot be in the future");
+        }
+    }
+}
