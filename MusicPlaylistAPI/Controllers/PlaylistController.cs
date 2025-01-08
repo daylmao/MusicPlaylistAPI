f027329d2798428cc7949a03d0d3331a0cf07fde
Dayron <dayronbp06@gmail.com>
2025-01-07 22:50:42 -0400

    add controllers

---
+++ MusicPlaylistAPI/Controllers/PlaylistController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicPlaylistAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistController : ControllerBase
+    {
+    }
+}
