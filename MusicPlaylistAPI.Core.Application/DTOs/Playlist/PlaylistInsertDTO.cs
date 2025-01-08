82636be15d713b3f36179d9b03e168597bee88ac
Dayron <dayronbp06@gmail.com>
2025-01-07 22:50:23 -0400

    update playlistInsertDTO

---
+++ MusicPlaylistAPI.Core.Application/DTOs/Playlist/PlaylistInsertDTO.cs
@@ -0,0 +1,17 @@
+﻿using MusicPlaylistAPI.Core.Application.DTOs.Song;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlaylistAPI.Core.Application.DTOs.Playlist
+{
+    public class PlaylistInsertDTO
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; } = null;
+        public ICollection<SongDTO>? Songs { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
