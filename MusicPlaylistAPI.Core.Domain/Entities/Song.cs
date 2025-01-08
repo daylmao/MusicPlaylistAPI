6c670bf39dff1868ca68af5d7fe420a76d05c256
Dayron <dayronbp06@gmail.com>
2025-01-07 22:45:12 -0400

    update song entity

--- MusicPlaylistAPI.Core.Domain/Entities/Song.cs
+++ MusicPlaylistAPI.Core.Domain/Entities/Song.cs
@@ -10,8 +10,8 @@ namespace MusicPlaylistAPI.Core.Domain.Entities
     {
         public Guid SongId { get; set; }
         public string? Title { get; set; }
-        public string Artist { get; set; } = string.Empty;
-        public string Genre { get; set; } = string.Empty;
+        public string? Artist { get; set; }
+        public string? Genre { get; set; } 
         public float Duration { get; set; }
         public virtual ICollection<Playlist> Playlists { get; set; } = new List<Playlist>();
     }
