10d5ccb069b154deb38fe74e9ab42621ef22539e
Dayron <dayronbp06@gmail.com>
2025-01-12 16:42:45 -0400

    update dto

--- MusicPlaylistAPI.Core.Application/DTOs/Song/SongDTO.cs
+++ MusicPlaylistAPI.Core.Application/DTOs/Song/SongDTO.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace MusicPlaylistAPI.Core.Application.DTOs.Song
 {
     public class SongDTO
     {
+        public Guid SongId { get; set; }
         public string? Artist { get; set; }
         public string? Title { get; set; }
         public string? Genre { get; set; }
