4e57eec60203894b233773d449e5a12bdc185cf1
Dayron <dayronbp06@gmail.com>
2025-01-16 12:23:43 -0400

    add email configuration

---
+++ MusicPlaylistAPI.Core.Application/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,14 @@
+﻿using MusicPlaylistAPI.Core.Application.DTOs.Email;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlaylistAPI.Core.Application.Interfaces.Services
+{
+    public interface IEmailService
+    {
+        Task EmailRequest(EmailDTO email);
+    }
+}
