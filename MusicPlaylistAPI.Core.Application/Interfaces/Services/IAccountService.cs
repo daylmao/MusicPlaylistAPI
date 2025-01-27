21af71adc46bccf2186cd174f0f19a49942f32ae
Dayron <dayronbp06@gmail.com>
2025-01-27 17:18:05 -0400

    add identity account

---
+++ MusicPlaylistAPI.Core.Application/Interfaces/Services/IAccountService.cs
@@ -0,0 +1,14 @@
+﻿using MusicPlaylistAPI.Core.Application.DTOs.Account.Authenticate;
+
+namespace MusicPlaylistAPI.Core.Application.Interfaces.Services
+{
+    public interface IAccountService
+    {
+        Task<AuthenticateResponseDTO> AuthenticateAsync(AuthenticateRequestDTO request);
+        Task<string> ConfirmAccountAsync(string userId, string token);
+        Task<ForgotPasswordResponseDTO> ForgotPasswordAsync(ForgotPasswordRequestDTO request, string origin);
+        Task<RegisterResponseDTO> RegisterAsync(RegisterRequestDTO request, string origin);
+        Task<ResetPasswordResponseDTO> ResetPasswordAsync(ResetPasswordRequestDTO request, string token);
+        Task SignOut();
+    }
+}
